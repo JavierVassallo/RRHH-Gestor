a2ca361c7e16f887228be9c6dde709883064656d
JavierVassallo <54564164+JavierVassallo@users.noreply.github.com>
2020-05-14 13:49:16 -0300

    Update settings.py

--- GestorRRHH/settings.py
+++ GestorRRHH/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'RRHH',
         'USER':'root',
-        'PASSWORD':'root',
+        'PASSWORD':'',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
